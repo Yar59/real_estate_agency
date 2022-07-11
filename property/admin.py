6bcca0888cb45c5e551a6ec9a7f6dfd529e5af28
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-07-11 14:48:06 +0300

    add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 
 @admin.register(Flat)
@@ -11,3 +11,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_display_links = ('address',)
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+
+
+@admin.register(Complaint)
+class FlatAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat')
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -54,3 +55,24 @@ class Flat(models.Model):
     class Meta:
         verbose_name = 'Квартира'
         verbose_name_plural = 'Квартиры'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name='Кто пожаловался',
+        related_name='complaints',
+        null=True,
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.CASCADE,
+        verbose_name='Квартира, на которую пожаловались',
+        related_name='complaints',
+    )
+    text = models.TextField('Текст жалобы')
+
+    class Meta:
+        verbose_name = 'Жалоба'
+        verbose_name_plural = 'Жалобы'
