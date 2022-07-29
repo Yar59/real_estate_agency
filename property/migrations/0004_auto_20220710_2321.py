6f24471d39c0ebec9cd7610a395abafe5d49005f
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-07-29 12:26:32 +0300

    simplify code

--- property/migrations/0004_auto_20220710_2321.py
+++ property/migrations/0004_auto_20220710_2321.py
@@ -5,8 +5,7 @@ from django.db import migrations
 
 def set_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    Flat.objects.filter(construction_year__gt=2015).update(new_building=True)
-    Flat.objects.filter(construction_year=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
     Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
